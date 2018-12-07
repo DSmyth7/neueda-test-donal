1bf7dea051dcc9a0fed75bff9b3c31bf3ef06615
DSmyth7 <d-jsmyth@hotmail.co.uk>
2018-12-07 14:43:11 +0000

    Created Animal and Whale classes.

---
+++ NeuedaTest/src/code/Animal.java
@@ -0,0 +1,17 @@
+package code;
+
+public abstract class Animal {
+
+	private String name;
+
+	public Animal() {
+		super();
+	}
+
+	public Animal(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+}
